b2c7c184af8c81c61b0a25d1b2860f8dc2eb4f67
Leo de Saboia <leo-saboia@VRP65046399.acad.unifor.br>
2025-07-10 18:17:30 -0300

    1 hora aula 3

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,9 +1,11 @@
 // Barrel file (um arquivo que reexporta todos os arquivos da pasta schema)
+import { audioChunks } from "./audio-chunks.ts"
 import { questions } from "./questions.ts"
 import { rooms } from "./rooms.ts"
 
 
 export const schema = {
     rooms,
-    questions
+    questions,
+    audioChunks
 }
